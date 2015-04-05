8e640dd3ed69c2648c94461b385667f797018220
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2015-04-05 15:08:52 -0500

    Framework for handling multiple file types by mime type.
    * Create a FileData struct that we will use for indexing.
    ** The MimeType
    ** The IndexTime
    ** The FileName
    ** The FullPath
    ** The RelativePath based off of the rootPath.
    ** The TextContent
    * Detect plain text and image formats as well as pdfs and get
      from each in whatever way is most appropriate.

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"mime"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
+	"strings"
+	"time"
 
 	lpt "gopkg.in/GeertJohan/go.leptonica.v1"
 	gts "gopkg.in/GeertJohan/go.tesseract.v1"
@@ -21,6 +26,11 @@ import (
   2. We need to implement the full text indexing of the documents.
   3. We need a standard way to query the documents.
   4. Flesh out a way to handle various file types easily.
+
+  Indexing:
+    1. Path based off of a root path.
+    2. mime-type
+    3.
 */
 
 func defaultTessData() (possible string) {
@@ -34,6 +44,7 @@ func defaultTessData() (possible string) {
 var tessData = flag.String("tess_data_prefix", defaultTessData(), "Location of the tesseract data")
 var help = flag.Bool("help", false, "Show this help")
 var pdfDensity = flag.Int("pdfdensity", 300, "density to use when converting pdf's to tiffs")
+var rootPath = flag.String("root_path", "", "Root path for indexing")
 
 func getPixImage(f string) (*lpt.Pix, error) {
 	log.Print("extension: ", filepath.Ext(f))
@@ -57,13 +68,16 @@ func getPixImage(f string) (*lpt.Pix, error) {
 	return lpt.NewPixFromFile(f)
 }
 
-func main() {
-	flag.Parse()
+type FileData struct {
+	RelativePath string
+	FullPath     string
+	FileName     string
+	MimeType     string
+	IndexTime    time.Time
+	Text         string
+}
 
-	if *help {
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
+func ocrImageFile(file string) (string, error) {
 	// Create new tess instance and point it to the tessdata location.
 	// Set language to english.
 	t, err := gts.NewTess(filepath.Join(*tessData, "tessdata"), "eng")
@@ -72,24 +86,92 @@ func main() {
 	}
 	defer t.Close()
 
+	pix, err := getPixImage(file)
+	if err != nil {
+		return "", fmt.Errorf("Error while getting pix from file: %s (%s)", file, err)
+	}
+	defer pix.Close()
+
+	t.SetPageSegMode(gts.PSM_AUTO_OSD)
+
+	// TODO(jwall): What is this even?
+	err = t.SetVariable("tessedit_char_whitelist", ` !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_abcdefghijklmnopqrstuvwxyz{|}~`+"`")
+	if err != nil {
+		return "", fmt.Errorf("Failed to set variable: %s\n", err)
+	}
+
+	t.SetImagePix(pix)
+
+	return t.Text(), nil
+}
+
+func getPlainTextContent(file string) (string, error) {
+	fd, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	bs, err := ioutil.ReadAll(fd)
+	if err != nil {
+		return "", err
+	}
+	return string(bs), nil
+}
+
+func ProcessFile(file string) (*FileData, error) {
+	// TODO(jeremy): Use hashing to detect if we've already indexed this file.
+	ext := filepath.Ext(file)
+	// TODO(jwall): Do I want to do anything with the params?
+	mt, _, err := mime.ParseMediaType(mime.TypeByExtension(ext))
+	parts := strings.SplitN(mt, "/", 2)
+	fd := FileData{
+		MimeType:     mt,
+		FileName:     filepath.Base(file),
+		FullPath:     path.Clean(file),
+		RelativePath: strings.Replace(filepath.Dir(file), path.Clean(*rootPath)+"/", "", 1),
+		IndexTime:    time.Now(),
+	}
+	log.Printf("mime category: %q", parts[0])
+	// TODO(jeremy): We need an abstract file type handler interface and
+	// a way to register them.
+	switch parts[0] {
+	case "text":
+		fd.Text, err = getPlainTextContent(file)
+		if err != nil {
+			return nil, err
+		}
+	case "application":
+		if strings.ToLower(ext) == ".pdf" {
+			fd.Text, err = ocrImageFile(file)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, fmt.Errorf("Unhandled file format %q", mt)
+		}
+	case "image":
+		fd.Text, err = ocrImageFile(file)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("Unhandled file format %q", mt)
+	}
+	return &fd, nil
+}
+
+func main() {
+	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
 	for _, file := range flag.Args() {
 		// TODO(jwall): Handle pdf
-		pix, err := getPixImage(file)
+		fd, err := ProcessFile(file)
 		if err != nil {
-			log.Fatalf("Error while getting pix from file: %s (%s)", file, err)
+			fmt.Printf("Error reading file %q, %v\n", file, err)
 		}
-		defer pix.Close()
-
-		t.SetPageSegMode(gts.PSM_AUTO_OSD)
-
-		// TODO(jwall): What is this even?
-		err = t.SetVariable("tessedit_char_whitelist", ` !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_abcdefghijklmnopqrstuvwxyz{|}~`+"`")
-		if err != nil {
-			log.Fatalf("Failed to set variable: %s\n", err)
-		}
-
-		t.SetImagePix(pix)
-
-		fmt.Println(t.Text())
+		fmt.Println(fd)
 	}
 }
